03af085a2b736d21eb30b678f04fbfc0afd012b9
Saasolution-sl <slsaasolution@gmail.com>
2026-08-31 23:51:22 +0200

    Add Next.js configuration for remote images

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+      remotePatterns: [
+            {
+                    protocol: "https",
+                            hostname: "raw.githubusercontent.com",
+                                    pathname: "/Saasolution-sl/clinica-avanzada/**",
+                                          },
+                                              ],
+                                                },
+                                                };
+
+                                                export default nextConfig;
+                                                
